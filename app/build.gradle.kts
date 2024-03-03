c4c1d2890c650553c155e43dd268c4cf3f7d0ceb
Pratham Batra <pratham11.batra@gmail.com>
2024-03-03 13:41:47 +0530

    Added permission handling for location access and function to request location data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.test.weather"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.test.weather"
--- app/src/main/java/com/test/weather/MainActivity.kt
+++ app/src/main/java/com/test/weather/MainActivity.kt
@@ -1,26 +1,73 @@
 package com.test.weather
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.location.Location
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Granularity
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.permissionx.guolindev.PermissionX
 import com.test.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        if(!isLocationEnabled()){
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
+        PermissionX.init(this)
+            .permissions(Manifest.permission.ACCESS_FINE_LOCATION)
+            .onForwardToSettings { scope, deniedList ->
+                scope.showForwardToSettingsDialog(deniedList, "Please grant permission to access" +
+                        " precise location in App Settings", "OK", "Cancel")
+            }
+            .request { allGranted, _, deniedList ->
+                if (allGranted) {
+                    if(isLocationEnabled()){
+                        requestLocationData()
+                    }
+                    else{
+                        showEnableLocationDialog()
+                    }
+                }else{
+                    Toast.makeText(this, "Permission Denied: $deniedList", Toast.LENGTH_LONG).show()
+                }
+            }
+
+    }
+
+    private fun showEnableLocationDialog() {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Location Services Disabled")
+        alertDialog.setMessage("Please enable location services to use this feature.")
+        alertDialog.setPositiveButton("Enable") { _, _ ->
+            // Open the device settings to enable location
+            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(settingsIntent)
+            finish()
         }
-        else{
-
+        alertDialog.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
         }
-
+        alertDialog.show()
     }
 
     private fun isLocationEnabled() : Boolean{
@@ -29,5 +76,28 @@ class MainActivity : AppCompatActivity() {
 
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData(){
+        val mLocationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY,1000).apply {
+            setMinUpdateDistanceMeters(200F)
+            setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+            setWaitForAccurateLocation(true)
+        }.build()
+
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper())
+    }
+
+    private val mLocationCallback = object : LocationCallback(){
+        override fun onLocationResult(p0: LocationResult) {
+            val mLastLocation: Location? = p0.lastLocation
+            val latitude = mLastLocation?.latitude
+            val longitude = mLastLocation?.longitude
+
+            Log.i("Latitude: ","$latitude")
+            Log.i("Longitude: ","$longitude")
+
+        }
     }
 }
